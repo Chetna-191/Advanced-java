e8032bb22baa7ad967f56ff5b8fc96cfba9b6cd3
Chetna-26 <mohantychetna191@gmail.com>
2026-03-15 00:21:39 +0530

    Problem4 Plagiarism Detection using n-grams

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Problem4PlagiarismDetector {
+
+    public static Set<String> generateNGrams(String text, int n) {
+
+        String[] words = text.split(" ");
+        Set<String> ngrams = new HashSet<>();
+
+        for (int i = 0; i <= words.length - n; i++) {
+            String gram = "";
+            for (int j = 0; j < n; j++) {
+                gram += words[i + j] + " ";
+            }
+            ngrams.add(gram.trim());
         }
+
+        return ngrams;
     }
-}
+
+    public static void main(String[] args) {
+
+        String doc1 = "machine learning is fun and powerful";
+        String doc2 = "machine learning is fun and easy";
+
+        Set<String> n1 = generateNGrams(doc1, 3);
+        Set<String> n2 = generateNGrams(doc2, 3);
+
+        n1.retainAll(n2);
+
+        System.out.println("Matching n-grams: " + n1.size());
+    }
